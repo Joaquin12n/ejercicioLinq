ad3239d53315fcda8cbaa1db303d09ff972ef0ae
Joaquin12n <joaquinvegaedu@gmail.com>
2021-10-18 20:55:57 -0300

    ahi va el ejemplo

---
+++ Models/Menu.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace mvc.Models{
+
+    public class Menu{
+
+        public string Nombre =" al proyecto final  ";
+        public string dibujarVista(){
+            return "Bienvenido "+ Nombre;
+        }
+    }
+}
